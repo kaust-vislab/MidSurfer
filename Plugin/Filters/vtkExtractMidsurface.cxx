16e5630adfbf0de919a97206ebac0871e36a4303
Thomas Theussl <thomas.theussl@kaust.edu.sa>
2024-03-28 08:59:34 +0100

    -

--- Plugin/Filters/vtkExtractMidsurface.cxx
+++ Plugin/Filters/vtkExtractMidsurface.cxx
@@ -299,6 +299,7 @@ void vtkExtractMidsurface::ExtractMidsurface(vtkImageData *image, vtkPolyData *m
 	{
 		vtkNew<vtkZipperTriangulation> zipper;
 		zipper->SetInputConnection(append->GetOutputPort());
+		zipper->SetZipperAlpha(2.0);
 		zipper->Update();
 		mesh->DeepCopy(zipper->GetOutput());
 	}
